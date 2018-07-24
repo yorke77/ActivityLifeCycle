72471ba31c45195a0ba876fba68f99144cd34fa7
yorke77 <yorke77@163.com>
2018-07-25 00:24:42 +0800

    Initial commit

---
+++ app/src/main/java/com/yorke/activitylifecycle/DialogActivity.java
@@ -0,0 +1,16 @@
+package com.yorke.activitylifecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class DialogActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+}
---
+++ app/src/main/java/com/yorke/activitylifecycle/MainActivity.java
@@ -0,0 +1,75 @@
+package com.yorke.activitylifecycle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        Button btnStartNormalActivity = findViewById(R.id.start_normal_activity);
+        Button btnStartDialogActivity = findViewById(R.id.start_dialog_activity);
+
+        btnStartNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStartDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+}
